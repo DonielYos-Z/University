aaf2ed07f54e61a02001334220f31e07a949c881
DonielYos-Z <donyoszaner@gmail.com>
2025-07-09 11:26:25 +0300

    Update Login.cs

--- UniversityApp/Forms/PublicPages/Login.cs
+++ UniversityApp/Forms/PublicPages/Login.cs
@@ -18,10 +18,18 @@ namespace UniversityApp
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        private string HashPassword(string password)
         {
-
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                // Convert the byte array to a string.
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                    builder.Append(bytes[i].ToString("x2"));
+                return builder.ToString();
+            }
         }
         private void button1_Click(object sender, EventArgs e)
         {
