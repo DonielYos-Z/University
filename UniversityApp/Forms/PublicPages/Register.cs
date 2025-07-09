af6426007e59ab054921a1d2b4b40d7df10d7ba2
DonielYos-Z <donyoszaner@gmail.com>
2025-07-09 11:25:55 +0300

    Update Register.cs

--- UniversityApp/Forms/PublicPages/Register.cs
+++ UniversityApp/Forms/PublicPages/Register.cs
@@ -16,6 +16,19 @@ namespace UniversityApp
     {
         Form form;
         private string filepath = "users.txt";
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                // Convert the byte array to a string.
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                    builder.Append(bytes[i].ToString("x2"));
+                return builder.ToString();
+            }
+        }
         public Register(Login Login)
         {
             this.form = Login;
